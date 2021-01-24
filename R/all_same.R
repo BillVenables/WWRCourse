5ae8ed26ba473c301a89f90141d8ee406a3d6cba
BillVenables <Bill.Venables@gmail.com>
2021-01-24 11:17:31 +1000

    adding all_same

---
+++ R/all_same.R
@@ -0,0 +1,27 @@
+#' Compare multiple objects
+#'
+#' Simple function to allow comparisons between multiple objects
+#'
+#' @param x an object
+#' @param ... additional objects to be compared to \code{x}
+#'
+#' @return if all objects are equal to \code{x} as defined by
+#'         \code{all.equal(x, object)} yielding \code{TRUE} then
+#'         the logical value \code{TRUE}.  If not, then a
+#'         logical vector indicating which are, or are not,
+#'         equal to \code{x} in this sense
+#' @export
+#'
+#' @examples
+#' all_same(letters, LETTERS, sort(sample(letters)))
+#' all_same(letters, tolower(LETTERS), sort(sample(letters)))
+all_same <- function(x, ...) {
+  same <- sapply(list(...), function(dot) isTRUE(all.equal(x, dot)))
+  asame <- all(same)
+  if(asame) {
+    asame
+  } else {
+    structure(c(TRUE, same),
+              names = sapply(match.call(), deparse)[-1])
+  }
+}
